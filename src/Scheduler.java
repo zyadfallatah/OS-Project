41a281650c739b2520e8f4391e3d1c953f2aec99
zyadfallatah <zyadfallatah@gmail.com>
2024-05-11 16:59:39 +0300

    Fcfs is now a standalone components with the same logic + firstCome() now calls the logic inside Fcfs class

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,6 +1,7 @@
 package src;
 
 import components.RoundRobin.RoundRobinScreen;
+import components.Fcfs.Fcfs;
 
 /*
   Guidelines:
@@ -21,69 +22,14 @@ public class Scheduler {
   /* 
     **Don't Delete This just replace what's between ' '**
     @Author: 'abdulrahman '
-    @Components: 'Write your component names here' 
+    @Components: 'Fcfs' 
   */
- // to get the numbr of proces 
-  public static int NumOfprocess(){ 
-    Scanner input = new Scanner(System.in);//Create an object to get  input from the user
-    System.out.print("Enter the number of processes: ");
-    return input.nextInt();
- }
-
-// to get Burst Tims 
-public static int[] BurstT(int number) {
-    Scanner input = new Scanner(System.in);//Create an object to get  input from the user
-    //we need to assign array to get burst time for the each process 
-    int burstt[] = new int[number];
-    for(int i = 0; i < number; i++){ //get burst time for  each process 
-        System.out.print("Enter the burst time for p" + (i+1) + ": ");
-        burstt[i] = input.nextInt();
-     }
-    return burstt;
-   }
-
-/// to get Waiting Times for  each process
-public static int[] WaitingT(int[] burst, int number){
-    //now we need to assign array to get waiting  time for the each process 
-    int waiit[] = new int[number];
-    // process one soulde  be 0... 
-    waiit[0] = 0;// first process waiting  time 
-    int waitt = 0;// assign the veriable to count waiting  time
-    for(int i = 1; i < number; i++) {
-        waitt += burst[i-1];
-        waiit[i] = waitt;
-     }
-    return waiit;
-   }
-/// to get the average Turnaround Time 
-public static double AvgTurnaroundT(int[] turnaroundTime, int number) {
-    float sumtat = 0;///assign the veriable to count total Turnaround Time
-    for(int i = 0; i < number; i++) {
-        sumtat += turnaroundTime[i];//
-     }
-    return sumtat / number;
-  }
-/// to get the average waiting time
-public static double AvgWaitingT(int[] wait, int number){
-    float sumwt = 0; ///assign the veriable to count total Waiting Time
-    //Use a loop to calculate Turnaround Time
-    for(int i = 0; i < number; i++) {
-        sumwt += wait[i];
-      }
-    return sumwt / number;//to get the average waiting time
-     }
-//To print the final results
-public static void print(int[] burst, int[] waiit, int[] turnaroundTime, int number){
-    System.out.println("Process\t\tBurst Time\tWaiting Time\tTurnaround Time");
-    for(int i = 0; i < number; i++) {
-        turnaroundTime[i] = waiit[i] + burst[i];//To calculate turnaround Time
-        System.out.println("P" + (i+1) + "\t\t" + burst[i] + "\t\t" + waiit[i] + "\t\t" + turnaroundTime[i]);
-      }
-    System.out.println("Average TurnaroundTime time is:\t\t\t" + AvgTurnaroundT(turnaroundTime, number));
-    System.out.println("Average Waiting time is:\t\t\t" + AvgWaitingT(waiit, number));
-             
-     }  
-    System.out.println("First come First Serve under work...");
+  // to get the number of process 
+  Screen.clearScreen();
+  int numberOfProcess = Fcfs.NumOfprocess();
+  int[] burstT = Fcfs.BurstT(numberOfProcess);
+  int[] WaitingT = Fcfs.WaitingT(burstT, numberOfProcess);
+  Fcfs.print(burstT, WaitingT, numberOfProcess);
   }
 
   private static void shortestJob() {
